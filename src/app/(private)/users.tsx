dc77066621b8739f02cc729ee9e72cfe4b8a6c39
LuizFelipeBuaszczyk <163165579+LuizFelipeBuaszczyk@users.noreply.github.com>
2026-09-19 14:03:03 -0300

    fix(ui): link button component

--- src/app/(private)/users.tsx
+++ src/app/(private)/users.tsx
@@ -1,18 +1,33 @@
-import { Link } from "expo-router";
-import { ScrollView, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { ScrollView, Text, StyleSheet, View } from "react-native";
+
+import LinkButton from "@/components/LinkButton/LinkButton";
 
 
 export default function Users(){
     return (
         <ScrollView>
-            <Text>Users</Text>
-            <TouchableOpacity>
-                <Link href="/(private)/users/create">Create</Link>
-            </TouchableOpacity>
+            <Text style={styles.title}>Users</Text>
+            <View style={styles.viewActions}>
+               <LinkButton 
+                title="Create"
+                path='/(private)/users/create'
+               /> 
+            </View>
        </ScrollView>
     );
 }
 
 const styles = StyleSheet.create({
-
+    title: {
+        fontSize: 32,
+        textAlign: 'center',
+        margin: 10
+    },
+    viewActions: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: 10,
+    }
 });
